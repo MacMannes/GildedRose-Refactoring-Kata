327584c378e13c4a89d824b6859540b6ce023000
André Mathlener <andre.mathlener@ns.nl>
2025-04-21 11:11:47 +0200

    refactor: extract method isSulfuras

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -12,7 +12,7 @@ export class Item {
     update() {
         if (!this.isAgedBrie() && !this.isBackstagePass()) {
             if (this.quality > 0) {
-                if (this.name != 'Sulfuras, Hand of Ragnaros') {
+                if (!this.isSulfuras()) {
                     this.quality = this.quality - 1;
                 }
             }
@@ -33,14 +33,14 @@ export class Item {
                 }
             }
         }
-        if (this.name != 'Sulfuras, Hand of Ragnaros') {
+        if (!this.isSulfuras()) {
             this.sellIn = this.sellIn - 1;
         }
         if (this.sellIn < 0) {
             if (!this.isAgedBrie()) {
                 if (!this.isBackstagePass()) {
                     if (this.quality > 0) {
-                        if (this.name != 'Sulfuras, Hand of Ragnaros') {
+                        if (!this.isSulfuras()) {
                             this.quality = this.quality - 1;
                         }
                     }
@@ -53,6 +53,10 @@ export class Item {
                 }
             }
         }
+    }
+
+    private isSulfuras() {
+        return this.name == 'Sulfuras, Hand of Ragnaros';
     }
 
     private isAgedBrie() {
