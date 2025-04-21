86744abcadddb764079be6eecf3f37a5e59c5c34
André Mathlener <andre.mathlener@ns.nl>
2025-04-21 10:29:49 +0200

    style: use 4 space indentation

--- TypeScript/test/vitest/approvals.spec.ts
+++ TypeScript/test/vitest/approvals.spec.ts
@@ -12,19 +12,19 @@ import { Item, GildedRose } from '@/gilded-rose';
  */
 
 describe('Gilded Rose Approval', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
+    it('should foo', () => {
+        const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+        const items = gildedRose.updateQuality();
 
-    expect(items).toMatchSnapshot();
-  });
+        expect(items).toMatchSnapshot();
+    });
 
-  it('should thirtyDays', () => {
-    const consoleOutput = execSync(
-      'ts-node test/golden-master-text-test.ts 30',
-      { encoding: 'utf-8' }
-    );
+    it('should thirtyDays', () => {
+        const consoleOutput = execSync(
+            'ts-node test/golden-master-text-test.ts 30',
+            { encoding: 'utf-8' },
+        );
 
-    expect(consoleOutput).toMatchSnapshot();
-  });
+        expect(consoleOutput).toMatchSnapshot();
+    });
 });
